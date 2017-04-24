df814a800197d40cb7823220450c13b32a2771a3
Andrew Gurung <andrewgurung@users.noreply.github.com>
2017-04-24 13:30:07 -0400

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <!-- the router outlet where all matched components will be view -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
-  name: 'app',
-  components: {
-    Hello
-  }
+  name: 'app'
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,31 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import the vue instance
 import Vue from 'vue'
+// import the App components
 import App from './App'
+// import the vue router
+import VueRouter from 'vue-router'
+// tell vue to use the router
+Vue.use(VueRouter)
+//define your routes
+const routes = []
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+// Create the router instance and pass the `routes` option
+// You can pass in additional options here.
+const router = new VueRouter({
+  routes, // short cut for routes: routes
+  mode: 'history'
 })
+
+// instantiate the vue instance
+new Vue({
+  // define the root element
+  el: '#app',
+  // pass the template to the root component
+  template: '<App/>',
+  // declare components that the root component can alert-success
+  components: { App },
+  // pass in the router to the Vue instance
+  router
+}).$mount('#app')
